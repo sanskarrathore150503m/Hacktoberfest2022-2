2771ec1d99a1d8ff40f9d3994448738e9973b97e
sanskarrathore150503m <115772981+sanskarrathore150503m@users.noreply.github.com>
2022-10-15 01:04:10 +0530

    Pattern.cpp
    Program to print pattern like X. Please merge my program with label Hacktoberfest

---
+++ Pattern.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>  
+using namespace std;
+
+int main()
+ {  
+  int n,i,j,m;  
+  cout<<"Enter half of length of pattern X: ";  
+  cin>>n;  
+  m=2*n-1;  
+  for(i=1;i<=m;i++)  
+  {  
+    for(j=1;j<=m;j++)  
+    {  
+       if(i==j || j==(m-i+1))  
+       {  
+          cout<<"*";   
+       else  
+       {  
+         cout<<" ";  
+         }  
+    }  
+    cout<<"\n";  
+  }  
+  return 0;  
+}
